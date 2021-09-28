8aa19737b220365f377e6e3cf1a7ab7749c3b35c
chdl1314 <88882964+chdl1314@users.noreply.github.com>
2021-09-28 17:32:11 +0800

    DOOM

--- Token.sol
+++ Token.sol
@@ -6,7 +6,7 @@ import "./TRC20Detailed.sol";
 
 contract Token is TRC20, TRC20Detailed {
 
-    constructor () public TRC20Detailed("YourTokenName", "YTN", 18) {
-        _mint(msg.sender, 10000000000 * (10 ** uint256(decimals())));
+    constructor () public TRC20Detailed("DOOM", "DOOM", 6) {
+        _mint(msg.sender, 8888 * (10 ** uint256(decimals())));
     }
 }
